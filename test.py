85237fdcef2c92db3a1be6eee7806fc9be38c1cc
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2022-11-26 15:43:04 +0200

    Updated .gitignore

--- test.py
+++
@@ -1,9 +0,0 @@
-import io
-def doSomethingCool(i,other):
-    i.write(b"BRUH")
-    if other:
-        doSomethingCool(i,False)
-with open("temp.txt","wb+") as f:
-    doSomethingCool(f,True)
-    f.seek(0,0)
-    print(f.read())
--- test/printfTest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void printSmth(){
-    printf("%d",1);
-}
-int main(){
-    //printf("%d",1);
-    return 0;
-}
